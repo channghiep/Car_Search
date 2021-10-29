e305ea44cd02a617d43b75b5a8a87b4cae57234c
channghiep <48778072+channghiep@users.noreply.github.com>
2021-10-29 15:59:57 -0700

    api fetch edited

--- src/CrazyTags.jsx
+++ src/CrazyTags.jsx
@@ -29,9 +29,10 @@ const CrazyTags = () => {
   // just a name I made up for allowing dynamic changes for tagify settings on this component
   const [tagifySettings, setTagifySettings] = useState([])
   const [tagifyProps, setTagifyProps] = useState({})
-  const [searchVal, setSearchVal] = useState([{"value":"a"}])
-  const [obj, setObj] = useState()
-  console.log(tagifyProps)
+  const [searchVal, setSearchVal] = useState([])
+  const [objectArray, setobjectArray] = useState([])
+  const [carList, setCarList] = useState([])
+  // console.log(tagifyProps)
   // on component mount
   useEffect(() => {
     // setTagifyProps({loading: true})
@@ -76,72 +77,194 @@ const CrazyTags = () => {
     tagifyRef1.current && tagifyRef1.current.removeAllTags()
   }
   // console.log(searchVal[1])
-  console.log(typeof searchVal)
-  console.log(searchVal)
-  console.log(searchVal.slice(1,-1))
-  let objectArray = [];
-
-  if(searchVal.length > 2){
-    let jsonArray = searchVal.slice(1,-1).split(",")
-    console.log(jsonArray)
-    let i = 0;
-
-    while(i < jsonArray.length){
-      console.log(JSON.parse(jsonArray[i]));
-      let tempObj = JSON.parse(jsonArray[i])
-      // tempObj.type = 'afdsf'
-      
-      //Define the tag
-      ///vehicles/GetMakesForVehicleType/car?format=json---Vehicle type
-      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/${tempObj.value}?format=json`)
-        .then(response => {
-          console.log(response.data.Count)
-          if(response.data.Count == 0){
-            Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${tempObj.value}?format=json`)
-              .then(response =>{
-                if(response.data.Count  !=0){
-                  tempObj.type = 'make'
-                }else{
-                  tempObj.type = 'year'
-                }
-              })
-          }else{
-            tempObj.type = 'vehicletype'
-          }
-          // console.log(response.data)
-        })
-        console.log(tempObj)
-        objectArray.push(tempObj);
-        // setObj(objectArray);
-      
-      ///vehicles/GetVehicleTypesForMake/mercedes?format=json--Vehicle name
-      
-      i++;
-    }
-    console.log(objectArray)
-  }
-
+  // console.log(typeof searchVal)
+  // console.log(searchVal)
+  // console.log(searchVal.slice(1,-1))
+  // let  = [];
   useEffect(()=>{
-    // Axios.get()
-    let make;
-    let type;
-    let year;
-    let i = 0;
-    console.log(objectArray[i].value)
-    while(i<objectArray.length){
-      console.log(objectArray[i])
-      if(objectArray[i].type == 'make'){
-        make = `${objectArray[i].value}`
-      }else if(objectArray[i].type == 'vehicletype'){
-        type = `${objectArray[i].value}`
-      }else{
-        year =  `${objectArray[i].value}`
-      }
-      i++
-    }
-    console.log(make)
+    if(searchVal.length > 2){
+      let jsonArray = searchVal.slice(1,-1).split(",")
+      console.log(jsonArray.length)
+      const verifyfunc = async () =>{
+        if(jsonArray.length > 0){
+          let i = 0;
+      
+          while(i < jsonArray.length){
+            // console.log(JSON.parse(jsonArray[i]));
+            let tempObj = JSON.parse(jsonArray[i])
+            // tempObj.type = 'afdsf'
+            console.log(Boolean(tempObj))
+            
+            //Define the tag
+            ///vehicles/GetMakesForVehicleType/car?format=json---Vehicle type
+            
+            if(tempObj){
+              const response = await Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/${tempObj.value}?format=json`);
+            if(response.data.Count === 0){
+              await Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${tempObj.value}?format=json`)
+                    .then(res =>{
+                      if(res.data.Count  !=0){
+                        tempObj.type = 'make'
+                        console.log(tempObj.type)
+                      }else{
+                        tempObj.type = 'year'
+                        // console.log(tempObj.type)
+                      }
+                    })
+            }else{
+              tempObj.type = 'vehicletype'
+            }
+            // await Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/${tempObj.value}?format=json`)
+            //   .then(response => {
+            //     console.log(response.data.Count)
+            //     if(response.data.Count === 0){
+            //       await Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${tempObj.value}?format=json`)
+            //         .then(res =>{
+            //           if(res.data.Count  !=0){
+            //             tempObj.type = 'make'
+            //             console.log(tempObj.type)
+            //           }else{
+            //             tempObj.type = 'year'
+            //             // console.log(tempObj.type)
+            //           }
+            //         })
+            //     }else{
+            //       tempObj.type = 'vehicletype'
+            //     }
+                console.log(tempObj)
+                objectArray[i]=tempObj;
+                console.log(objectArray)
+            //     // console.log(response.data)
+            function fetchData(){
+              console.log(objectArray)
+              let make;
+              let type;
+              let year;
+              let i = 0;
+              console.log(make)
+              // console.log(objectArray[5].type)
+              // console.log(objectArray)
+              // console.log(objectArray.length)
+              if(objectArray.length > 0){
+                console.log(objectArray[0].type)
+                while(i<objectArray.length){
+                  console.log(i)
+                  console.log(objectArray[i].value)
+                  if(objectArray[i].type === 'make'){
+                    make = `${objectArray[i].value}`
+                  }else if(objectArray[i].type == 'vehicletype'){
+                    type = `${objectArray[i].value}`
+                  }else{
+                    year = `${objectArray[i].value}`
+                  }
+                  i++
+                }
+                console.log(make)
+                if(make !== undefined){
+                  if(type !== undefined){
+                    if(year !== undefined){
+                      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}/vehicletype/${type}?format=json`).then(res=>{
+                        console.log(res.data.Results)
+                        setCarList(res.data.Results)
+                      })
+                      console.log(make)
+                      console.log(carList)
+                    }else{
+                      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/vehicletype/${type}?format=json`).then(res=>{
+                        console.log(res.data.Results)
+                        setCarList(res.data.Results)
+                      })
+                      console.log(make)
+                      console.log(carList)
+                    }
+                  }else{
+                    if(year !== undefined){
+                      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}?format=json`).then(res=>{
+                        console.log(res.data.Results)
+                        setCarList(res.data.Results)
+                      })
+                      console.log(make)
+                      console.log(carList)
+                    }else{
+                      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${make}?format=json`).then(res=>{
+                        console.log(res.data.Results)
+                        setCarList(res.data.Results)
+                      })
+                      console.log(make)
+                      console.log(carList)
+                    }
+                  }
+                }else{
+                  if(type !== undefined){
+                    if(year !== undefined){
+                      console.log('not support')
+                    }else{
+                      Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/${type}?format=json`).then(res=>{
+                      console.log(res.data.Results)
+                      setCarList(res.data.Results)
+                      })
+                      console.log(make)
+                      console.log(carList)
+                    }
+                  }
+                }
 
-  },[objectArray])
+              }
+              
+            }
+            fetchData()
+            
+            //   })
+            }
+              // console.log(tempObj)
+              
+              // objectArray.push(tempObj);
+              // setObj(objectArray);
+            
+            ///vehicles/GetVehicleTypesForMake/mercedes?format=json--Vehicle name
+            
+            i++;
+          }
+        }
+      }
+      verifyfunc()
+     
+      }else{
+        setCarList([])
+      }
+  },[searchVal])
+
+  
+  // useEffect(()=>{
+  //   // Axios.get()
+  //   let make;
+  //   let type = "vehicletype/"
+  //   let year = "modelyear/"
+  //   let i = 0;
+  //   // console.log(objectArray[5].type)
+  //   // console.log(objectArray)
+  //   // console.log(objectArray.length)
+  //   if(objectArray.length > 0){
+  //     // console.log(objectArray[0].type)
+  //     while(i<objectArray.length){
+  //       console.log(i)
+  //       console.log(objectArray[i].value)
+  //       if(objectArray[i].type === 'make'){
+  //         make = `${objectArray[i].value}`
+  //       }else if(objectArray[i].type == 'vehicletype'){
+  //         type = type + `${objectArray[i].value}`
+  //       }else{
+  //         year = year + `${objectArray[i].value}`
+  //       }
+  //       i++
+  //     }
+  //   }
+  //   Axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${make}?format=json`).then(res=>{
+  //     console.log(res.data.Results)
+  //   })
+  //   console.log(make)
+
+  // },[objectArray.length])
     
 
   // let tagArray = JSON.parse(searchVal.slice(1,-1).split(","))
@@ -165,7 +288,7 @@ const CrazyTags = () => {
       <Tags
         tagifyRef={tagifyRef1}
         settings={settings}
-        defaultValue="a,b,c"
+        // defaultValue="a,b,c"
         autoFocus={true}
         {...tagifyProps}
         onChange={onChange}
@@ -182,7 +305,15 @@ const CrazyTags = () => {
         onDropdownUpdated={() => console.log("onDropdownUpdated")}
       />
 
-
+      <div>
+        {carList.map((car)=>{
+          return(
+            <div>
+              {car.Model_Name}
+            </div>
+          )
+        })}
+      </div>
 
     </>
   )
